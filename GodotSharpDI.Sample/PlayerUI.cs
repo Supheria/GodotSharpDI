1bd15b8c0fa0184be1b420e74f3d0b26f6508b7e
Supheria <supheria@gmail.com>
2026-02-02 22:05:04 +0800

    添加示例项目（初步）

---
+++ GodotSharpDI.Sample/PlayerUI.cs
@@ -0,0 +1,27 @@
+﻿using Godot;
+using GodotSharpDI.Abstractions;
+
+namespace GodotSharpDI.Sample;
+
+[User]
+public sealed partial class PlayerUI : Control, IServicesReady
+{
+    [Inject]
+    private IPlayerStats PlayerStats
+    {
+        set => GD.Print("PlayerUI inject Player Stats");
+    }
+
+    [Inject]
+    private IGameState GameState
+    {
+        set => GD.Print("PlayerUI inject Game State");
+    }
+
+    void IServicesReady.OnServicesReady()
+    {
+        GD.Print("PlayerUI updated after services ready");
+    }
+
+    public override partial void _Notification(int what);
+}
